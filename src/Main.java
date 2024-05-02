aee52cf48050a39b631b9f2b1958b91476c91a07
BuildTools <unconfigured@null.spigotmc.org>
2024-05-01 21:32:14 -0400

    added test 100

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test 2");
+        System.out.println("test 1");
+        System.out.println("test 100");
     }
 }
