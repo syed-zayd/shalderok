0ef6f89c908d38737fb2f6332a572aaf7df69c77
BuildTools <unconfigured@null.spigotmc.org>
2024-05-01 21:35:50 -0400

    added hello world

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test 1");
         System.out.println("test 100");
+
+        System.out.println("hello world");
     }
 }
