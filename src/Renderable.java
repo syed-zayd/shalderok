2991721de1014f391491bdc704d54e018a1736a0
tiwari-aryan <aryan.tiwari0204@gmail.com>
2024-05-03 12:24:02 -0400

    stuff

--- src/Renderable.java
+++ src/Renderable.java
@@ -1,4 +1,4 @@
 // interface for everything that can be rendered
 interface Renderable {
-    public void paint();
+    public void paint(Graphics2D g2d);
 }
---
+++ src/Weapon.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+
+public class Weapon implements Renderable {
+
+    public void paint(Graphics2D g2d){
+
+    }
+
+}
