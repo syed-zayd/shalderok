81f62ea69cba1c4ee572b1892c40fd90f79730a1
King-Of-Cold <zaydsyed9@gmail.com>
2024-05-09 14:00:23 -0400

    53t45t

---
+++ src/Bow.java
@@ -0,0 +1,29 @@
+public class Bow extends Weapon {
+
+    private 
+
+    @Override
+    public int getAtk() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAtk'");
+    }
+
+    @Override
+    public int getDodge() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDodge'");
+    }
+
+    @Override
+    public int getSpd() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpd'");
+    }
+
+    @Override
+    public void attack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'attack'");
+    }
+
+}
--- src/Projectile.java
+++ src/Projectile.java
@@ -1,5 +1,6 @@
 
 public abstract class Projectile extends WorldObject {
+
     
     
     public Projectile(String spriteFilePath, double x, double y, int w, int h) {
